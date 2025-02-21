6973c263c1db108f3e376125a830905f8579bff2
fabian <fabianantoniososatorrealba854@gmail.com>
2025-02-21 15:16:08 -0300

    primercomit

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,7 @@
+const producto = 'Monitor 20 Pulgadas';
+const precio = '30 USD ';
+
+//console.log(producto.concat(precio));
+//console.log(producto.concat('En descuento'));
+
+console.log( producto + "Con un precio de: " + precio);
---
+++ 03-Strings/js/04-app.js
@@ -0,0 +1,13 @@
+const producto = '                       Monitor 20 pulgadas                         ';
+console.log(producto);
+console.log(producto.length);
+
+
+// Eliminar el inicio 
+console.log( producto.trimStart() );
+console.log( producto.trimEnd().trimEnd() );
+
+
+
+
+console.log( producto.trim() )
